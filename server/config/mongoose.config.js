4820015cdb2163690c8b6152863531b0ed543bbb
John Wilder <80861346+JohnWilder406@users.noreply.github.com>
2021-08-04 08:16:23 -0700

    changes

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const dbName = "";
+const dbName1 = process.env.DB_NAME1;
+const dbName2 = process.env.DB_NAME2;
 
-mongoose.connect("mongodb://127.0.0.1/" + dbName, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log(`Established a connection to the "${dbName}" database`))
-    .catch(err => console.log(`Something went wrong when connecting to the "${dbName}" database`, err))
+var conn1 = mongoose.createConnection("mongodb://127.0.0.1/" + dbName1, {useNewUrlParser: true, useUnifiedTopology: true})
+var conn2 = mongoose.createConnection("mongodb://127.0.0.1/" + dbName2, {useNewUrlParser: true, useUnifiedTopology: true})
+
+module.exports = {conn1, conn2}
