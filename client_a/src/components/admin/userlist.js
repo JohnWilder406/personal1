15bcd776abfff829d15a57a0eead20c3539cb75e
John Wilder <80861346+JohnWilder406@users.noreply.github.com>
2021-08-05 08:09:27 -0700

    Router setup

---
+++ client_a/src/components/admin/userlist.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const UserList = (props) => {
+    return (
+        <h1>Admin Main Page</h1>
+    )
+}
+
+export default UserList;
---
+++ client_a/src/views/users/main.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const UserMain = (props) => {
+    return (
+        <h1>User Main Page</h1>
+    )
+}
+
+export default UserMain;
